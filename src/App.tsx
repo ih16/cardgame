136ec8c9344f210dcd5c3622a73aba2bc7c2e7d2
Tonmoy_Strativ <tonmoy@strativ.se>
2021-07-02 16:11:34 +0600

    chores: -Built a grid layout placing 4 players around and a board in the middle -Implement card shuffling and dealing

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,101 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+
 import './App.css';
 
-function App() {
+import { deck, shuffle } from './utility';
+interface Cards {
+  suit: string;
+  rank: string;
+  priority: number;
+  points: number;
+}
+interface Players {
+  north: Array<Cards>;
+  west: Array<Cards>;
+  east: Array<Cards>;
+  south: Array<Cards>;
+}
+
+const initialPlayer: Players = {
+  north: [],
+  west: [],
+  east: [],
+  south: [],
+};
+
+const App: React.FC = () => {
+  const [mainDeck, setMainDeck] = useState(shuffle(deck));
+  const [playerDeck, setPlayerDeck] = useState(initialPlayer);
+
+  const renderMaindCards = mainDeck.map(card => {
+    return (
+      <div key={`${card.suit}${card.rank}`} className="card">
+        <div className={`suit-${card.suit}`}>{card.suit}</div>
+        <div className="rank">{card.rank}</div>
+      </div>
+    );
+  });
+  const renderPlayerCards = (cards: Array<Cards>) =>
+    cards.map(card => {
+      return (
+        <div key={`${card.suit}${card.rank}`} className="card">
+          <div className={`suit-${card.suit}`}>{card.suit}</div>
+          <div className="rank">{card.rank}</div>
+        </div>
+      );
+    });
+  function dealCards() {
+    setPlayerDeck({
+      ...playerDeck,
+      north: [...playerDeck.north, ...mainDeck.slice(0, 4)],
+      west: [...playerDeck.west, ...mainDeck.slice(4, 8)],
+      south: [...playerDeck.south, ...mainDeck.slice(8, 12)],
+      east: [...playerDeck.east, ...mainDeck.slice(12, 16)],
+    });
+    setMainDeck(mainDeck.slice(16, mainDeck.length));
+  }
+  function resetCards() {
+    setPlayerDeck(initialPlayer);
+    setMainDeck(shuffle(deck));
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="north-west"></div>
+      <div className="north">
+        <div className="player-tag">North</div>
+        {renderPlayerCards(playerDeck.north)}
+      </div>
+      <div className="north-east"></div>
+      <div className="east">
+        <div className="player-tag">East</div>
+        {renderPlayerCards(playerDeck.east)}
+      </div>
+      <div className="middle">
+        <div className="mid-card-container">{renderMaindCards}</div>
+        <div className="button-container">
+          <button className="primary-button" onClick={resetCards}>
+            Shuffle
+          </button>
+          {!!mainDeck.length && (
+            <button className="primary-button" onClick={dealCards}>
+              Deal
+            </button>
+          )}
+        </div>
+      </div>
+      <div className="west">
+        <div className="player-tag">West</div>
+        {renderPlayerCards(playerDeck.west)}
+      </div>
+      <div className="south-west"></div>
+      <div className="south">
+        <div className="player-tag">South</div>
+        {renderPlayerCards(playerDeck.south)}
+      </div>
+      <div className="south-east"></div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/utility/index.ts
@@ -0,0 +1,236 @@
+
+interface Cards {
+  suit: string,
+  rank: string,
+  priority: number,
+  points: number
+}
+
+export const deck : Array<Cards> = [{
+    suit: "♣",
+    rank: "7",
+    priority: 1,
+    points : 0,
+  
+  },
+  {
+    suit: "♣",
+    rank: "8",
+    priority: 2,
+    points : 0,
+  
+  },
+  {
+    suit: "♣",
+    rank: "Q",
+    priority: 3,
+    points : 0,
+  
+  },
+  {
+    suit: "♣",
+    rank: "K",
+    priority: 4,
+    points : 0,
+  
+  },
+  {
+    suit: "♣",
+    rank: "10",
+    priority: 5,
+    points : 1,
+  
+  },
+  {
+    suit: "♣",
+    rank: "A",
+    priority: 6,
+    points : 1,
+  
+  },
+  {
+    suit: "♣",
+    rank: "9",
+    priority: 7,
+    points : 2,
+  
+  },
+  {
+    suit: "♣",
+    rank: "J",
+    priority: 8,
+    points : 3,
+  
+  },
+  {
+    suit: "♦",
+    rank: "7",
+    priority: 1,
+    points : 0,
+  
+  },
+  {
+    suit: "♦",
+    rank: "8",
+    priority: 2,
+    points : 0,
+  
+  },
+  {
+    suit: "♦",
+    rank: "Q",
+    priority: 3,
+    points : 0,
+  
+  },
+  {
+    suit: "♦",
+    rank: "K",
+    priority: 4,
+    points : 0,
+  
+  },
+  {
+    suit: "♦",
+    rank: "10",
+    priority: 5,
+    points : 1,
+  
+  },
+  {
+    suit: "♦",
+    rank: "A",
+    priority: 6,
+    points : 1,
+  
+  },
+  {
+    suit: "♦",
+    rank: "9",
+    priority: 7,
+    points : 2,
+  
+  },
+  {
+    suit: "♦",
+    rank: "J",
+    priority: 8,
+    points : 3,
+  
+  },
+  {
+    suit: "♠",
+    rank: "7",
+    priority: 1,
+    points : 0,
+  
+  },
+  {
+    suit: "♠",
+    rank: "8",
+    priority: 2,
+    points : 0,
+  
+  },
+  {
+    suit: "♠",
+    rank: "Q",
+    priority: 3,
+    points : 0,
+  
+  },
+  {
+    suit: "♠",
+    rank: "K",
+    priority: 4,
+    points : 0,
+  
+  },
+  {
+    suit: "♠",
+    rank: "10",
+    priority: 5,
+    points : 1,
+  
+  },
+  {
+    suit: "♠",
+    rank: "A",
+    priority: 6,
+    points : 1,
+  
+  },
+  {
+    suit: "♠",
+    rank: "9",
+    priority: 7,
+    points : 2,
+  
+  },
+  {
+    suit: "♠",
+    rank: "J",
+    priority: 8,
+    points : 3,
+  
+  },
+  {
+    suit: "♥",
+    rank: "7",
+    priority: 1,
+    points : 0,
+  
+  },
+  {
+    suit: "♥",
+    rank: "8",
+    priority: 2,
+    points : 0,
+  
+  },
+  {
+    suit: "♥",
+    rank: "Q",
+    priority: 3,
+    points : 0,
+  
+  },
+  {
+    suit: "♥",
+    rank: "K",
+    priority: 4,
+    points : 0,
+  
+  },
+  {
+    suit: "♥",
+    rank: "10",
+    priority: 5,
+    points : 1,
+  
+  },
+  {
+    suit: "♥",
+    rank: "A",
+    priority: 6,
+    points : 1,
+  
+  },
+  {
+    suit: "♥",
+    rank: "9",
+    priority: 7,
+    points : 2,
+  
+  },
+  {
+    suit: "♥",
+    rank: "J",
+    priority: 8,
+    points : 3,
+  
+  }];
+
+
+export const shuffle = (deck : Array<Cards>) : Array<Cards> => deck.map(a => ({ sort: Math.random(), value: a })).sort((a, b) => a.sort - b.sort).map(a => a.value);
+
